6117672e43255b47de9a938501502e6360f26c13
usmslm102 <usmslm102@users.noreply.github.com>
2025-02-06 13:23:33 +0000

    deploy: e15ecf3aa97bfb29bc2c70e9aa119914aadb9dbf

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/whatsend/sw.js', { scope: '/whatsend/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/WhatSend/sw.js', { scope: '/WhatSend/' })})}
